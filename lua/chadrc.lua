eb0f852709d40d7e5dcea85ce19f888e07dc47da
Logan Teder <lteder01@gmail.com>
2024-10-28 17:01:03 -0400

    `chadrc` fix for UI v3.0

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,19 +1,11 @@
 -- This file needs to have same structure as nvconfig.lua 
--- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
+-- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
 	theme = "penumbra_dark",
-
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
-}
-
-M.ui = {
   transparency = true
 }
 
